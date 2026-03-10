c0e2683339b5c822719c6bfa1ad7fd55a40fc4db
Jormaedes Luis <jluis@c2r7s1.42luanda.com>
2026-03-10 11:22:19 +0100

    feat(HashMap): adicionar o arquivo HashMap
    Criei o arquivo HashMap e criei a classs HashMap e o seu construtor

---
+++ HashMap.js
@@ -0,0 +1,8 @@
+class HashMap {
+	constructor(loadFactor=0.75, capacity=16) {
+		this.loadFactor = loadFactor;
+		this.capacity = capacity;
+		this.buckets = new Array(this.capacity).fill(null).map(()=>([]));
+		this.size = 0;
+	}
+}
