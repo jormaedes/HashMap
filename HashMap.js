f327cd4d1ee77493d536bd2101893d1c671e130f
Jormaedes Luis <jluis@c2r7s1.42luanda.com>
2026-03-10 11:24:24 +0100

    feat(HashMap): adicionar funcao hash

--- HashMap.js
+++ HashMap.js
@@ -1,8 +1,17 @@
 class HashMap {
-	constructor(loadFactor=0.75, capacity=16) {
+	constructor(loadFactor = 0.75, capacity = 16) {
 		this.loadFactor = loadFactor;
 		this.capacity = capacity;
-		this.buckets = new Array(this.capacity).fill(null).map(()=>([]));
+		this.buckets = new Array(this.capacity).fill(null).map(() => ([]));
 		this.size = 0;
+	}
+
+	hash(key) {
+		let hashCode = 0;
+		const primeNumber = 31;
+		for (let i = 0; i < key.length; i++) {
+			hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+		}
+		return hashCode;
 	}
 }
