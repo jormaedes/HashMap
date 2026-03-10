68b5c67da7e8c1805ad7f4b1f202f5b1ee1230dc
Jormaedes Luis <jluis@c2r7s1.42luanda.com>
2026-03-10 12:17:51 +0100

    feat(HashSet): adicionar o metodo add

--- HashSet.js
+++ HashSet.js
@@ -2,6 +2,10 @@ class HashSet {
 	constructor(loadFactor = 0.75, capacity = 16) {
 		this._map = new HashMap(loadFactor, capacity);
 	}
+
+	add(key) {
+		this._map.set(key, true);
+	}
 }
 
 export { HashSet };
